3a8200e0095df07aa4ac8fe222b5f2c359860892
unknown <chanjs.vsh@gmail.com>
2025-08-01 13:17:19 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$page = $_GET['page'] ?? 'home';
+$pagePath = "pages/{$page}.php";
+$stylePath = "styles/{$page}.css";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo ucfirst($page)?></title>
+    <?php 
+    echo "<link rel='stylesheet' href='styles/header.css'>";
+    echo "<link rel='stylesheet' href='styles/head.css'>";
+    echo "<link rel='stylesheet' href='styles/footer.css'>";
+    ?>
+</head>
+<body>
+    <?php
+    include "header.php";
+
+    include $pagePath;
+
+    include "footer.php";
+    ?>
+</body>
+</html>
